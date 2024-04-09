be392b96ad987f4c9167499a99de14ef8f19d10c
Udayabharathim <160568654+Udayabharathim@users.noreply.github.com>
2024-04-09 21:26:03 +0530

    Update Exp4_Dflipflop.v

--- Exp4_Dflipflop.v
+++ Exp4_Dflipflop.v
@@ -1,18 +1,11 @@
-module sr_ff(clk,q,rst,s,r); 
-input s,r,clk,rst; 
+module d_ff(clk,q,rst,d); 
+input d,clk,rst; 
 output reg q; 
 always@(posedge clk) 
 begin 
 if(rst==1) 
-q=1'b0; 
+q=1'b0;  
 else 
-begin 
-case({s,r}) 
-2'b00:q=q; 
-2'b01:q=1'b0; 
-2'b10:q=1'b1; 
-2'b11:q=1'bx; 
-endcase 
-end 
+q=d; 
 end 
 endmodule
