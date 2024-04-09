21ba2f681b8d2a6f716ec13b5fbd7e3400526dac
Udayabharathim <160568654+Udayabharathim@users.noreply.github.com>
2024-04-09 21:27:15 +0530

    Update Exp4_JKflipflop.v

--- Exp4_JKflipflop.v
+++ Exp4_JKflipflop.v
@@ -1,8 +1,18 @@
-6(ii): JK FLIP FLOP 
-module jk( j,k,clk,q); 
-input j,k,clk; 
-output reg q=0; 
-
-
-
+module jk_ff(clk,q,rst,j,k); 
+input j,k,clk,rst; 
+output reg q; 
+always@(posedge clk) 
+begin 
+if(rst==1) 
+q=1'b0; 
+else 
+begin 
+case({j,k}) 
+2'b00:q=q; 
+2'b01:q=1'b0; 
+2'b10:q=1'b1; 
+2'b11:q=~q; 
+endcase 
+end 
+end 
 endmodule
