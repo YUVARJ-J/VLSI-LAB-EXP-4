de81224fed25a49eb410d5fd48466eefff8217e1
Udayabharathim <160568654+Udayabharathim@users.noreply.github.com>
2024-04-09 21:32:38 +0530

    Update Exp4_Tflipflop.v

--- Exp4_Tflipflop.v
+++ Exp4_Tflipflop.v
@@ -1,8 +1,14 @@
-6(iv):T FLIPFLOP 
-module tff(t,clk,q); 
-input t,clk; 
-output reg q=1; 
-
-
-
-endmodule
+module t_ff(clk,q,rst,t); 
+input t,clk,rst; 
+output reg q; 
+always@(posedge clk) 
+begin 
+if(rst==1) 
+q=1'b0; 
+else 
+if(t==0) 
+q=q; 
+else 
+q=~q; 
+end 
+endmodule 
